e7930bf06757ecc6dcdd748f86a1f34098777c84
lucciii33 <angelomaiele@gmail.com>
2022-07-18 21:00:20 +0000

    contact us page

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -7,6 +7,19 @@ import { Context } from "../store/appContext";
 export const ContactUs = () => {
 	const { store, actions } = useContext(Context);
 
+	const boxVarient = {
+		hidden: {
+			x: "100vh",
+		},
+		visible: {
+			x: 0,
+			duration: 2,
+			// transition: {
+			// 	delay: 0.3
+			// }
+		}
+	}
+
 	return (
 		<div className="contai bg-ligth">
 			<div className="texts">
@@ -27,10 +40,41 @@ export const ContactUs = () => {
 
 						<textarea className="mensaje mt-3" placeholder="Tell us about your project/product" type="text"></textarea>
 					</div>
+					<div className="mt-2">
+
+						<input type="checkbox" className="checkbox-round"></input>
+						<span>I have read and accept the <strong className="strong">Terms of use </strong>& <strong className="strong">Privacy Policy</strong></span>
+					</div>
+					<div>
+						<button className="button-29 mt-2">Lest talk<i className="far fa-paper-plane ms-1"></i></button>
+					</div>
+
 				</div>
-				<div className="second-box">
-					<h2>cualquier cosa</h2>
-				</div>
+				<motion.div className="second-box" transition={{ type: "spring", stiffness: 130 }}
+					variants={boxVarient}
+					animate="visible"
+					initial="hidden">
+					<h2 className="text-center p-box">Usertive Solutions</h2>
+					<div>
+						<div className="d-flex">
+							<i className="far fa-envelope"></i>
+							<p className="text-contact-box">ilutioncode@gmail.com</p>
+						</div>
+						<div className="d-flex">
+							<i className="fas fa-map-marker-alt"></i>
+							<p className="text-contact-box">Miami, United State</p>
+						</div>
+						<div className="d-flex">
+							<i className="fas fa-mobile-alt"></i>
+							<p className="text-contact-box">9864984455</p>
+						</div>
+
+					</div>
+
+
+
+
+				</motion.div>
 			</div>
 
 		</div>
