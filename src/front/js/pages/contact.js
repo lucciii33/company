d2ec8150c1de81d8a8f24b69df2bf92a84ce8e04
lucciii33 <angelomaiele@gmail.com>
2022-07-25 17:02:45 +0000

    more

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion"
+import emailjs from 'emailjs-com';
 
 import { Context } from "../store/appContext";
 
@@ -20,6 +21,19 @@ export const ContactUs = () => {
 		}
 	}
 
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('gmail', 'service_d70o4b5', e.target, 'HASNJCXly2tABuH0x')
+			.then((result) => {
+				console.log(result.text);
+			}, (error) => {
+				console.log(error.text);
+			});
+
+		e.target.reset()
+	};
+
 	return (
 		<div className="contai bg-ligth">
 			<div className="texts">
@@ -29,53 +43,55 @@ export const ContactUs = () => {
 				<h6 className="title-contact-3">Fill out the enquiry form and we’ll get back to you as soon as possible.</h6>
 			</div>
 
-			<div className="d-flex conta-form">
-				<div>
-					<input className="email" placeholder="Email address" type="text"></input>
-					<div className="d-flex mt-3">
-						<input className="name" placeholder="Full Name" type="text"></input>
-						<input className="phone" placeholder="Phone" type="text"></input>
-					</div>
-					<div className="text-area">
-
-						<textarea className="mensaje mt-3" placeholder="Tell us about your project/product" type="text"></textarea>
-					</div>
-					<div className="mt-2">
-
-						<input type="checkbox" className="checkbox-round"></input>
-						<span>I have read and accept the <strong className="strong">Terms of use </strong>& <strong className="strong">Privacy Policy</strong></span>
-					</div>
+			<form onSubmit={sendEmail} >
+				<div className="d-flex conta-form">
 					<div>
-						<button className="button-29 mt-2">Lest talk<i className="far fa-paper-plane ms-1"></i></button>
-					</div>
+						<input className="email" placeholder="Email address" type="text" name="email"></input>
+						<div className="d-flex mt-3">
+							<input className="name" placeholder="Full Name" type="text" name="name"></input>
+							<input className="phone" placeholder="Phone" type="text" name="phone"></input>
+						</div>
+						<div className="text-area">
 
+							<input className="mensaje mt-3" placeholder="Tell us about your project/product" type="text" name="message"></input>
+						</div>
+						<div className="mt-2">
+
+							<input type="checkbox" className="checkbox-round"></input>
+							<span>I have read and accept the <strong className="strong">Terms of use </strong>& <strong className="strong">Privacy Policy</strong></span>
+						</div>
+						<div>
+							<button className="button-29 mt-2">Lest talk<i className="far fa-paper-plane ms-1"></i></button>
+						</div>
+
+					</div>
+					<motion.div className="second-box" transition={{ type: "spring", stiffness: 130 }}
+						variants={boxVarient}
+						animate="visible"
+						initial="hidden">
+						<h2 className="text-center p-box">Usertive Solutions</h2>
+						<div>
+							<div className="d-flex">
+								<i className="far fa-envelope"></i>
+								<p className="text-contact-box">ilutioncode@gmail.com</p>
+							</div>
+							<div className="d-flex">
+								<i className="fas fa-map-marker-alt"></i>
+								<p className="text-contact-box">Miami, United State</p>
+							</div>
+							<div className="d-flex">
+								<i className="fas fa-mobile-alt"></i>
+								<p className="text-contact-box">9864984455</p>
+							</div>
+
+						</div>
+
+
+
+
+					</motion.div>
 				</div>
-				<motion.div className="second-box" transition={{ type: "spring", stiffness: 130 }}
-					variants={boxVarient}
-					animate="visible"
-					initial="hidden">
-					<h2 className="text-center p-box">Usertive Solutions</h2>
-					<div>
-						<div className="d-flex">
-							<i className="far fa-envelope"></i>
-							<p className="text-contact-box">ilutioncode@gmail.com</p>
-						</div>
-						<div className="d-flex">
-							<i className="fas fa-map-marker-alt"></i>
-							<p className="text-contact-box">Miami, United State</p>
-						</div>
-						<div className="d-flex">
-							<i className="fas fa-mobile-alt"></i>
-							<p className="text-contact-box">9864984455</p>
-						</div>
-
-					</div>
-
-
-
-
-				</motion.div>
-			</div>
+			</form>
 
 		</div>
 	);
