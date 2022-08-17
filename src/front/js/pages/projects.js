296f64d201a499bd769d208968978c7aad57a12b
lucciii33 <angelomaiele@gmail.com>
2022-08-17 20:42:40 +0000

    more

--- src/front/js/pages/projects.js
+++ src/front/js/pages/projects.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { motion, useScroll } from "framer-motion"
+import elana from "../../img/captureElana.png";
 
 
 import { Context } from "../store/appContext";
@@ -71,7 +72,7 @@ export const Projects = () => {
 						variants={boxVarient3}
 						animate="visible"
 						initial="hidden">
-						<img src="https://i.pinimg.com/originals/46/c7/16/46c716ad6b833085403c062a28b50b83.jpg" className="img-project ms-3"></img>
+						<img src={elana} className="img-project1 ms-3"></img>
 						<h3 className="paragraph ms-3">Lorem Ipsum</h3>
 						<p className="description ms-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
 						<button className="button-29 ms-3">see me!</button>
