e9b655b49b24aff4d5825e8cf5120d3f5b5b8055
lucciii33 <angelomaiele@gmail.com>
2022-11-09 20:30:21 +0000

    stripe

--- src/front/js/component/checkoutButton.js
+++ src/front/js/component/checkoutButton.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 export const CheckOutButton = () => {
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+  //adds the event listener on initial render and cleans it up when finished.
+  useEffect(() => {}, []);
 
-    const { store, actions } = useContext(Context);
-    //adds the event listener on initial render and cleans it up when finished.
-    useEffect(() => {
-
-    }, []);
-
-    return (
-        <div className="">
-            <button className="btn btn-primary" onClick={() => actions.createOrder()}>Checkout</button>
-        </div>
-    );
+  return (
+    <div className="">
+      <button
+        className="btn btn-primary"
+        onClick={
+          () => actions.createOrder()
+          //   .then((data) => {
+          //     console.log(data);
+          //     return (window.location.href = data.url.url);
+          //     return navigate(`${data.url.url}`);
+          //   })
+        }
+      >
+        Checkout
+      </button>
+    </div>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,29 +1,36 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			cart: []
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      cart: [],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			createOrder: async () => {
-				try {
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/order/create-checkout-session")
-					const data = await resp.json()
-					setStore({ cart: data })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				} catch (error) {
-					console.log("Error loading message from backend", error)
-				}
-			},
-		}
-	};
+      createOrder: () => {
+        const store = getStore();
+        fetch(`${process.env.BACKEND_URL}/order/create-checkout-session`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: {},
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            // setStore({ cart: data });
+            // return data;
+            return (window.location.href = data.url);
+          })
+          .catch((err) => console.log(err));
+      },
+    },
+  };
 };
 
 export default getState;
