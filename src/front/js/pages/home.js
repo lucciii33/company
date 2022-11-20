4d094fbf0749e4cdb128276d108bd763fe0f1d81
lucciii33 <angelomaiele@gmail.com>
2022-11-20 22:09:12 +0000

    more

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,7 +2,9 @@ import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import main1 from "../../img/main1.png";
-import elana from "../../img/captureElana.png";
+import elana from "../../img/Logo.png";
+import elana1 from "../../img/figma.png";
+import elana2 from "../../img/elana.png";
 import "../../styles/home.css";
 import Chatboot from "../component/Chatboot";
 
@@ -130,19 +132,44 @@ export const Home = () => {
       </div>
       <div className="bg-banner-2">
         <h2 className="text-center text-gradient-home pt-5">Our last project</h2>
-        <div className="d-flex justify-content-evenly">
-          <div className="text-controller-2 my-auto px-5 ">
-            <h2 className="title-home-text">TEXT TITLE</h2>
-            <p className="paragraph-home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua. "There's one exception to the rule above: If you are using the display property to define your div as a flex container, then you can't use the text-align property to center text horizontally inside the div. Instead, you have to use the justify-content property with the value center."https://blog.hubspot.com/website/center-div-css</p>
-            <span><a>See project</a></span>
+        <div className="d-flex">
+          <div className="card-home m-5">
+            <div className="">
+              <img src={elana} className="home-image-project mt-4 ms-5"></img>
+            </div>
+            <div className="ms-3 mt-2 p-3">
+              <h6 className="card-title">Duration:</h6><span>1 YEAR</span>
+              <h6 className="card-title">Location:</h6><span>England-Uk, USA-Miami</span>
+              <h6 className="card-title">Description:</h6><span>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</span>
+            </div>
           </div>
+          <div className="icon-home">
+            <i class="fas fa-arrow-right"></i>
+          </div>
+
+          {/* <div className="card-home-screen m-5"> */}
+          <div className="bg-image-home d-block">
+            <div>
+              <img src={elana1} className="home-image-project-screen mt-4 ms-4"></img>
+            </div>
+            <div className="arrow-down">
+              <i class="fas fa-arrow-down"></i>
+            </div>
+
+          </div>
+          {/* </div> */}
+
+          {/* <div className="card-home-screen m-5"> */}
           <div className="">
-            <img src={elana} className="image-lastProject"></img>
+            <img src={elana2} className="home-image-project-screen mt-4 ms-5"></img>
           </div>
+          {/* </div> */}
+
         </div>
       </div>
       <div className="bg-banner-3">
         <h2>Banner 3 another cotact form</h2>
+
       </div>
       <div className="bg-banner-4">
         <h2>Banner four description of our bussiness </h2>
