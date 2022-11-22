0c9bc4341164d19d9eda210ff33676abebac701a
lucciii33 <angelomaiele@gmail.com>
2022-11-22 18:18:57 +0000

    more work

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,6 +7,9 @@ import elana1 from "../../img/figma.png";
 import elana2 from "../../img/elana.png";
 import "../../styles/home.css";
 import Chatboot from "../component/Chatboot";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -23,6 +26,15 @@ export const Home = () => {
   // useEffect(() => {
   //   handleShow();
   // }, []);
+  const renderSlides = () =>
+    [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+      <div>
+        <div>
+          <h3>Slide {num}</h3>
+          <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" style={{ width: "300px", heigth: "300px" }}></img>
+        </div>
+      </div>
+    ));
   console.log(store.language);
   return (
     <div>
@@ -168,12 +180,15 @@ export const Home = () => {
         </div>
       </div>
       <div className="bg-banner-3">
-        <h2>Banner 3 another cotact form</h2>
-        {/* <div className="tk-blob" style={{ time: 20, amount: 5, fill: "#56cbb9" }}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 747.2 726.7">
-            <path d="M539.8 137.6c98.3 69 183.5 124 203 198.4 19.3 74.4-27.1 168.2-93.8 245-66.8 76.8-153.8 136.6-254.2 144.9-100.6 8.2-214.7-35.1-292.7-122.5S-18.1 384.1 7.4 259.8C33 135.6 126.3 19 228.5 2.2c102.1-16.8 213.2 66.3 311.3 135.4z"></path>
-          </svg>
-        </div> */}
+
+
+
+        <Slider dots={false}
+          slidesToShow={1}
+          slidesToScroll={1}
+          autoplay={true}
+          autoplaySpeed={2000}>{renderSlides()}</Slider>
+
 
       </div>
       <div className="bg-banner-4">
