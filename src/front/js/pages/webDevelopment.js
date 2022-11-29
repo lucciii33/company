83163b80c3db8368126611e2b0a9a9c54462265b
lucciii33 <angelomaiele@gmail.com>
2022-11-29 02:09:22 +0000

    more work

--- src/front/js/pages/webDevelopment.js
+++ src/front/js/pages/webDevelopment.js
@@ -28,7 +28,7 @@ export const WebDevelopment = () => {
                         <p className="mt-4">Be it custom Web applications development and SaaS platforms, ecommerce website development or informative lead generation sites, our value proposition, when it comes to web development, is to build solutions by prioritizing the context and needs of the end-users, ensuring meaningful results.</p>
                         <p className="mt-4">Be it custom Web applications development and SaaS platforms, ecommerce website development or informative lead generation sites, our value proposition, when it comes to web development, is to build solutions by prioritizing the context and needs of the end-users, ensuring meaningful results.</p>
                     </div>
-                    <div className="d-flex mt-5">
+                    <div className="d-flex mt-5 m-5">
                         <div className="">
                             <div className="box-chart">
                                 <h6>more that 10<br /> projects in 2022</h6>
@@ -39,7 +39,7 @@ export const WebDevelopment = () => {
                         </div>
                         <div className="">
                             <div className="box-charts-three">
-                                <h6>more that 10<br /> projects in 2022</h6>
+                                <h6 className=" my-auto">more that 10<br /> projects in 2022</h6>
                             </div>
                             <div className="box-charts-four">
                                 <h6>more that 10<br /> projects in 2022</h6>
