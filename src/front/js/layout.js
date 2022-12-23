e6d91b229bc72ad8b6955b313a27c8558fb5e937
lucciii33 <angelomaiele@gmail.com>
2022-12-23 18:25:45 +0000

    google analytics

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
@@ -20,9 +20,19 @@ import { Intro } from "./pages/intro";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import "../fonts/RobloxBlackOutline.ttf";
+import ReactGA from "react-ga4";
 
 //create your first component
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag("event", "page_view", {
+      page_path: location.pathname + location.search + location.hash,
+      page_search: location.search,
+      page_hash: location.hash,
+    });
+  }, [location]);
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
