f9090eeb5c65894dd1f154714aa9ef396c5da205
lucciii33 <angelomaiele@gmail.com>
2023-01-27 00:17:40 +0000

    more work

--- src/front/js/component/OurVision.js
+++ src/front/js/component/OurVision.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OurVision = () => {
   return (
-    <div className="p-5 bg-light text-or">
+    <div className="padding-controller-our-vision bg-light text-or">
       <h1 className="text-dark text-center w-100">Our Vision</h1>
       <h4 className="m-0 px-5 pb-5 pt-3 text-center">
         We aim to create memorable experiences that help an companies and
